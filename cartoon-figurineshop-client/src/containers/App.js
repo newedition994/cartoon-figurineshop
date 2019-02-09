e61633f16c6c284637fb22fc85b2df1f41c44d86
Breyanna Vass <breyannavass@gmail.com>
2019-02-09 11:48:42 -0500

    Rendered the front end containers

--- cartoon-figurineshop-client/src/containers/App.js
+++ cartoon-figurineshop-client/src/containers/App.js
@@ -2,33 +2,28 @@ import React, { Component } from 'react';
 import Figurines from './Figurines.js'
 import './App.css'
 
-
-const figurines = [
-    {
-        name: "Okoye",
-        price: 12,
-        img_url: "https://www.amazon.com/Funko-Marvel-Panther-Limited-Bobblehead/dp/B07B4NF97M/ref=sr_1_21?s=toys-and-games&ie=UTF8&qid=1549726868&sr=1-21&keywords=black+panther+funko+pop",
-        size: 3
-    },
-    {
-        name: "MBaku",
-        price: 12,
-        img_url: "https://www.amazon.com/Funko-Pop-Marvel-Panther-MBaku-Collectible/dp/B07C7KNVB9/ref=sr_1_13?s=toys-and-games&ie=UTF8&qid=1549726868&sr=1-13&keywords=black+panther+funko+pop",
-        size: 3
-    },
-    {
-        name: "Shuri",
-        price: 12,
-        img_url: "https://www.amazon.com/Funko-Pop-Marvel-Panther-Collectible/dp/B07799VYN8/ref=sr_1_7?s=toys-and-games&ie=UTF8&qid=1549726868&sr=1-7&keywords=black+panther+funko+pop",
-        size: 3
-    }
-]
+const API_URL = process.env.REACT_APP_API_URL;
 
 class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            figurines: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(`${API_URL}/figurines`)
+            .then(response => response.json())
+            .then(figurines => this.setState({ figurines }))
+    }
+
     render() {
         return (
             <div className="App">
-                <Figurines figurines={figurines} />
+                <Figurines figurines={this.state.figurines} />
             </div>
         )
     }
--- cartoon-figurineshop-client/src/containers/Figurines.js
+++ cartoon-figurineshop-client/src/containers/Figurines.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import './Figurines.css';
 
 const Figurines = (props) => (
-    <div>
+    <div className="FigurineContainer">
         <h1>Figurines Component</h1>
         {props.figurines.map(figurine =>
-            <div>
+            <div key={figurine.id} className="FigurineCard">
                 <h3>{figurine.name}</h3>
                 <p>Price: ${figurine.price}</p>
-                <img src={figurine.img_url} alt={figurine.name} />
+                <img className="FigurineImg" src={figurine.img_url} alt={figurine.name} />
                 <p>Size: {figurine.size}</p>
             </div>
-
         )}
     </div>
 );

8f8ecba745cd0d8e1ea503ade7fd18669e0b5e0d
Breyanna Vass <breyannavass@gmail.com>
2019-02-08 21:08:53 -0500

    Finished api/figurine controller

---
+++ cartoon-figurineshop-api/app/controllers/api/figurines_controller.rb
@@ -0,0 +1,49 @@
+class Api::FigurinesController < ApplicationController
+
+    before :set_figurine, only: [:show, :update, :destroy]
+    
+    def index
+        render json: Figurine.all
+    end
+
+    def create
+        figurine = Figurine.new(figurine_params)
+        if figurine.save
+            render json: figurine
+        else
+            render json: (message: figurine.errors), status: 400 
+        end
+    end
+
+    def show
+        render json: @figurine
+    end
+
+    def update
+        if @figurine.update(figurine_params)
+            render json: @figurine
+        else
+            render json: (message: figurine.errors), status: 400 
+        end
+    end
+
+    def destroy
+        if @figurine.destroy
+            render json: ( message: "It was successfully deleted!!"), status: 204
+        else
+            render json: ( message: "Unable to destroy figurine"), status: 400
+        end
+    end
+
+
+    private
+
+    def set_figurine
+        @figurine = Figurine.find_by(id: params[:id])
+    end
+
+    def figurine_params
+        params.require(:figurine).permit(:name, :price, :img_url, :size)
+    end
+
+end
--- cartoon-figurineshop-api/app/controllers/api/figurinescontroller.rb
+++
@@ -1,8 +0,0 @@
-class Api::FigurinesController < ApplicationController
-
-    # figure out what the Route Error is within the server
-    def index
-        render json: Figurine.all
-    end
-
-end

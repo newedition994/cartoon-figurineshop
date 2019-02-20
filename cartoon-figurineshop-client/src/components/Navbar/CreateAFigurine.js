85062d2a13a19a5fea62830601575d2d699458e2
Breyanna Vass <breyannavass@gmail.com>
2019-02-19 22:42:49 -0500

    Designing the figurine page route

--- cartoon-figurineshop-client/src/components/Navbar/CreateAFigurine.js
+++ cartoon-figurineshop-client/src/components/Navbar/CreateAFigurine.js
@@ -3,4 +3,11 @@ import React from 'react'
 
 // see the firgurineForm on another page and route
 
+const MakeAFigurine = () =>
+    <div>
+
+    </div>
+
 // import the form into the page and then export it
+
+export default MakeAFigurine

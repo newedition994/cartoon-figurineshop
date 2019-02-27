93e57d4cf3a62701472840fe0a91b2af879eacc6
Breyanna Vass <breyannavass@gmail.com>
2019-02-26 21:36:46 -0500

    Reworked the Cost component

--- cartoon-figurineshop-client/src/components/Navbar/Cost.js
+++ cartoon-figurineshop-client/src/components/Navbar/Cost.js
@@ -2,22 +2,52 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { getFigurines } from '../../actions/figurines';
 import '../../containers/Figurines.css'
-// import FigurineDisplay from '../FigurinesDisplay';
+import FigurineDisplay from '../FigurinesDisplay';
 
 
 class Cost extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            figurine: [],
+            costsMore: true
+        }
+    }
+
+    sortByPrice() {
+        const { figurine } = this.state
+        let newPriceList = figurine
+        if (this.state.costsMore) {
+            newPriceList = figurine.sort((a, b) => a.price > b.price)
+        } else {
+            newPriceList = figurine.sort((a, b) => a.price < b.price)
+        }
+        this.setState({
+            costsMore: !this.state.costsMore,
+            figurine: newPriceList
+        })
+    }
+
     componentDidMount() {
-        this.props.getFigurines()
+        // debugger
+        const figurine = getFigurines
+        this.setState({
+            costsMore: true,
+            figurine: figurine
+        })
     }
 
 
 
     render() {
+        debugger
+        const { figurine } = this.state
         return (
             <div className="FigurineContainer">
                 {/* need assistance with writing the sort function that will work in this rendering */}
-                {/* {this.props.figurines.sort((a, b) => a.figurine.price > b.figurine.price).map(figurine => <FigurineDisplay key={figurine.id} figurine={figurine.price} />)} */}
+                {figurine.map(figurine => <FigurineDisplay key={figurine.id} figurine={figurine.price} />)}
+
             </div>
         )
     }

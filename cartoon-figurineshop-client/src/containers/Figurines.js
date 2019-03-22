5231743c56fc64df20ea346d44e71722f3ebd1b2
Breyanna Vass <breyannavass@gmail.com>
2019-03-22 17:48:31 -0400

    Office Hour suggestions

--- cartoon-figurineshop-client/src/containers/Figurines.js
+++ cartoon-figurineshop-client/src/containers/Figurines.js
@@ -10,52 +10,50 @@ class Figurines extends Component {
 
     constructor(props) {
         super(props)
-        // this.handleClick = this.handleClick.bind(this);
         this.state = {
-            figurines: [],
-            sortByName: false
+            sortByName: "none"
         };
-        this.sortByName = this.sortByName.bind(this);
     }
 
     componentDidMount() {
         this.props.getFigurines()
     }
 
-    sortByName = () => {
-        function compare(a, b) {
-            if (a.name > b.name) return 1
-
-            if (a.name < b.name) return -1
-
-            return 0
-        }
-        // why is this undefined
-        let { figurines } = this.state.figurines
-
-        return figurines.sort(compare)
-    }
-
     handleClick = () => {
-        // this.setState({ figurines => })
         // we implement the change in state 
 
-        console.log('Clicked happened');
-        this.setState({ figurines: [...this.props.figurines] }, () => {
-            console.log("FInished")
-            this.sortByName()
+        const newOrder = this.state.sortOrder === "size" ? "alpha" : "size"
+
+        this.setState({ sortOrder: newOrder }, () => {
+            console.log("new state is ", this.state, "sorted array is", this.sortByName(), "sorted by size", this.sortBySize())
         })
+    }
 
-        // display the return of figurines.sort(compare)
+    sortByName = () => {
+        return this.props.figurines.slice().sort((a, b) => {
+            const nameA = a.name
+            const nameB = b.name
 
+            if (nameA < nameB) { return -1 }
+            if (nameA > nameB) { return 1 }
+            return 0
+        })
+    }
+
+    sortBySize = () => {
+        this.props.figurines.slice().sort((a, b) => b.size - a.size)
     }
 
     render() {
+
+        const sortedFigurines = this.state.sortOrder === "size" ? this.sortByName() : this.sortBySize()
+
         return (
             <div className="FigurineContainer">
                 <h1 className="NamePlate">Figurines Web App</h1>
                 <button className="nameFilter" onClick={this.handleClick}>Toggle Sort Figurines by Name</button>
-                {this.props.figurines.map(figurine => <FigurineDisplay key={figurine.id} figurine={figurine} />)}
+                {/* Map Error that is unclear */}
+                {sortedFigurines.map(figurine => <FigurineDisplay key={figurine.id} figurine={figurine} />)}
             </div>
         )
     }

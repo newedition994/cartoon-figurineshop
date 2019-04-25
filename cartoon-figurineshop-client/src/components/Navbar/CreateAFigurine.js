f7fce248a69f37963046007f33537264e431d8a6
Breyanna Vass <breyannavass@gmail.com>
2019-04-25 12:59:33 -0400

    Add Figurine Display but property id undefined

--- cartoon-figurineshop-client/src/components/Navbar/CreateAFigurine.js
+++ cartoon-figurineshop-client/src/components/Navbar/CreateAFigurine.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
 import FigurineForm from '../../containers/FigurineForm';
+import FigurinesDisplay from '../../components/FigurinesDisplay';
 
 
 // see the firgurineForm on another page and route
 
 class MakeAFigurine extends Component {
 
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="form-display">
                 <FigurineForm />
+                {/*  display the new figurine */}
+                <FigurinesDisplay />
             </div>
         )
     }
 }
-
-
-// display the new figurine
 
 // import the form into the page and then export it
 
